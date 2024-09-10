469d0461d4f22db4d91659e721757e2d66c11c3a
Atik Ullah Khan <atikullah.murad@gmail.com>
2024-09-10 12:54:42 +0600

    Introduce a failing test to check pre-build job behavior

--- tests/calculation/calculation.test.ts
+++ tests/calculation/calculation.test.ts
@@ -5,6 +5,8 @@ describe("Calculation functions", () => {
     expect(add(1, 2)).toBe(3);
     expect(add(-1, -1)).toBe(-2);
     expect(add(0, 0)).toBe(0);
+
+    expect(add(1, 2)).toBe(4); // this will fail
   });
 
   test("subtract should correctly subtract two numbers", () => {
