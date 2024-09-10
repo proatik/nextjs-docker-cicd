a48ccb2c4feb8f95534bd39d3dd46f9d163c4d7e
Atik Ullah Khan <atikullah.murad@gmail.com>
2024-09-10 13:59:30 +0600

    Fix the background color issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import BasicInformation from "@/components/BasicInformation";
 
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-20">
+    <main className="flex min-h-screen flex-col items-center justify-between py-20 bg-zinc-950">
       <div className="z-10 w-full max-w-5xl grid grid-cols-2 gap-4 justify-between font-mono">
         <ClientComponent />
         <ServerComponent />
